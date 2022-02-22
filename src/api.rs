2a42f66eba215a020dc97a626088be7e0399c437
Aviram Hassan <aviramyhassan@gmail.com>
2022-02-22 17:19:45 +0200

    in case of fire

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+pub type ConnectionID = u64;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TCPConnected {
     pub port: u16,
@@ -11,7 +13,29 @@ pub struct TCPData {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct AgentError {
+    msg: String,
+}
+
+impl AgentError {
+    pub fn from_error<T>(error: T) -> AgentError 
+    where T: ToString {
+        AgentError {
+            msg: error.to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Event {
     Connected(TCPConnected),
     Data(TCPData),
+    Error(AgentError),
+    Done,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    pub connection_id: Option<ConnectionID>,
+    pub event: Event,
 }
