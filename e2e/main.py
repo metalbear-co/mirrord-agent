a94b49238bec98b99ffbeaf3d46342090767c6e6
Aviram Hassan <aviramyhassan@gmail.com>
2022-02-24 13:50:29 +0200

    ..

--- e2e/main.py
+++ e2e/main.py
@@ -1,76 +1,90 @@
+import json
+import random
+import string
 import sys
 import time
-import string
-import random
-import json
 
 import httpx
 from kubernetes import client, config, watch
 from pydantic import BaseModel
 
+
 class InfoMessage(BaseModel):
     content: str
+
 
 class ConnectedData(BaseModel):
     connection_id: int
     port: int
 
+
 class ConnectedMessage(BaseModel):
     content: ConnectedData
+
 
 class Data(BaseModel):
     connection_id: int
     data: str
 
+
 class DataMessage(BaseModel):
     content: Data
+
 
 class TCPEndedData(BaseModel):
     connection_id: int
 
+
 class TCPEndedMessage(BaseModel):
     content: TCPEndedData
 
+
 def create_pod_definition(container_id: str):
     return client.V1Pod(
-        metadata=client.V1ObjectMeta(
-            name="agentpod"
-        ),
+        metadata=client.V1ObjectMeta(name="agentpod"),
         spec=client.V1PodSpec(
             host_ipc=True,
             host_pid=True,
-            volumes=[client.V1Volume(
-                name="containerd",
-                host_path=client.V1HostPathVolumeSource(path='/run/containerd/containerd.sock')
-            )],
+            volumes=[
+                client.V1Volume(
+                    name="containerd",
+                    host_path=client.V1HostPathVolumeSource(
+                        path="/run/containerd/containerd.sock"
+                    ),
+                )
+            ],
             containers=[
                 client.V1Container(
                     name="mirrord-agent",
-                    image="ghcr.io/metalbear-co/mirrord-agent:main",
+                    image="local/mirrord-agent",
                     security_context=client.V1SecurityContext(privileged=True),
                     volume_mounts=[
                         client.V1VolumeMount(
                             name="containerd",
-                            mount_path="/var/run/containerd/containerd.sock"
+                            mount_path="/var/run/containerd/containerd.sock",
                         )
                     ],
-                    command=["./mirrord-agent",
-								"--container-id",
-								container_id,
-								"--ports",
-								"80"
-                    ]  
+                    command=[
+                        "./mirrord-agent",
+                        "--container-id",
+                        container_id,
+                        "--ports",
+                        "80",
+                    ],
                 )
-            ]
-        )
+            ],
+        ),
     )
+
 
 def generate_data():
     # generate random string
-    return ''.join(random.choice(string.ascii_letters) for i in range(10))
+    return "".join(random.choice(string.ascii_letters) for i in range(10))
+
 
 def send_request(service_url: str, data: str):
     return httpx.post(service_url, json={"data": data})
+
 
 def main():
     service_url = sys.argv[1]
@@ -79,13 +93,15 @@ def main():
     v1 = client.CoreV1Api()
 
     # Get container id
-    pods = v1.list_namespaced_pod("default", label_selector='app=nginx').items
-    container_id = pods[0].status.container_statuses[0].container_id.replace('containerd://', "")
+    pods = v1.list_namespaced_pod("default", label_selector="app=nginx").items
+    container_id = (
+        pods[0].status.container_statuses[0].container_id.replace("containerd://", "")
+    )
 
     # Create agentpod
     pod = create_pod_definition(container_id)
     v1.create_namespaced_pod("default", pod)
-    
+
     # Wait for agentpod to start
     for _ in range(60):
         pod = v1.read_namespaced_pod_status("agentpod", "default")
@@ -94,20 +110,20 @@ def main():
         time.sleep(1)
     else:
         raise Exception("Agent pod is not running")
-    
+
     # Send requests
     for _ in range(10):
         data = generate_data()
         r = send_request(service_url, data).request
 
     # Assert data
-    logs = v1.read_namespaced_pod_log(name='agentpod', namespace='default')
+    logs = v1.read_namespaced_pod_log(name="agentpod", namespace="default")
     messages = []
     count_tcp_ended = 0
     count_info_message = 0
-    count_connected =  0
+    count_connected = 0
     count_data = 0
-    for log in logs.split('\n'):
+    for log in logs.split("\n"):
         if not log:
             continue
         parsed = json.loads(log)
@@ -130,5 +146,6 @@ def main():
     # We can't guarantee if it might be more than 1 message per request.
     assert count_data > 10
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
