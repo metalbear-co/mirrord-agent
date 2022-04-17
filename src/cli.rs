6db457708254d77ee87db8348b3f2b07fb48cfc1
Aviram Hassan <aviramyhassan@gmail.com>
2022-04-17 17:31:38 +0000

    Refactor agent

--- src/cli.rs
+++ src/cli.rs
@@ -5,11 +5,19 @@ use clap::Parser;
 pub struct Args {
     /// Container id to get traffic from
     #[clap(short, long)]
-    pub container_id: String,
+    pub container_id: Option<String>,
 
-    /// Ports to get traffic from
-    #[clap(short, long)]
-    pub ports: Vec<u16>,
+    /// Port to use for communication
+    #[clap(short = 'l', long, default_value_t = 61337)]
+    pub communicate_port: u16,
+
+    /// Commuication timeout in seconds
+    #[clap(short = 't', long, default_value_t = 30)]
+    pub communication_timeout: u16,
+
+    /// Interface to use
+    #[clap(short = 'i', long, default_value = "eth0")]
+    pub interface: String,
 }
 
 pub fn parse_args() -> Args {
